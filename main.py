eac2e54abf1377af1f60e251fe930a7ccb69e62e
Kyle Strout <>
2021-05-12 07:20:58 +0000

    Added Feature for bot to do trivia with user. Used Trivia API

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import os
 import requests #allows bot to make http request to get data from api, returns json
 import json #easier to work with data returned
 import random
-from flask import request
-from replit import db
+from flask import Flask, request, Response
 from keep_alive import keep_alive
+from replit import db
 
 
 
@@ -14,45 +14,30 @@ from keep_alive import keep_alive
 #--header 'client-id: p3atoxiilwa9je4t03twaedr5877mf' \
 #--header 'Authorization: Bearer 2gbdx6oar67tqtcmt49t3wpcgycthx'
 
-
+# secrets, twitch tokens, global vars
 my_secret = os.environ['TOKEN']
 CID = os.environ['CLIENT_ID']
-TOKEN = os.environ['OATH_TOKEN']  
-TWITCH_SECRET = 'good'
+OATH_TOKEN = os.environ['OATH_TOKEN']  
+TWITCH_SECRET = 'blueberry'
 streamer_name = 'laevus'
 
-API_HEADERS = {
-  'client_ID': CID,
-  'client_secret': TOKEN,
-  "grant_type": 'client credentials'
-}
 
 client = discord.Client()
 
-def checkIfLive(): #this shit dont work
-  r = requests.post('https://discord.com/api/oauth2/authorize?client_id=839072169128493056&permissions=2148002880&scope=bot', API_HEADERS)
-  
-  keys = r.json()
+def is_user_live():
+    endpoint = 'https://api.twitch.tv/helix/streams'
+    my_headers = {
+        'Client-ID': CID,
+        'Authorization': f'Bearer {OATH_TOKEN}'
+    }
+    my_params = {'user_login': streamer_name}
+    response = requests.get(endpoint, headers=my_headers, params=my_params)
+    data = response.json()
+    print(data)
+    if len(data) == 0:
+        return False
 
-  print(keys)
-
-  headers = {
-    'Client-ID': CID,
-    'Authorization': 'Bearer ' + keys['access_token']
-  }
-
-  print(headers)
-  
-  stream = requests.get('https://api.twitch.tv/helix/streams?user_login=' + streamer_name, headers=headers)
-
-  stream_data = stream.json();
-
-  print(stream_data);
-  if len(stream_data['data']) == 1:
-    print(streamer_name + ' is live: ' + stream_data['data'][0]['title'] + ' playing ' + stream_data['data'][0]['game_name']);
-  else:
-      print(streamer_name + ' is not live');
-
+# gets a random quote
 def getQuote():
   response = requests.get('https://zenquotes.io/api/random')
   json_data = json.loads(response.text)
@@ -60,7 +45,31 @@ def getQuote():
   quote = json_data[0]['q'] + " -" + json_data[0]['a']
   return quote
 
+# gets random trivia data question
+def getTrivia():
+  url = "https://numbersapi.p.rapidapi.com/random/trivia"
+  querystring = {"json":"true","fragment":"true","max":"20","min":"10"}
+  headers = {
+    'x-rapidapi-key': "0acac537f2msh6f343358ba73dd4p125502jsn473a06834b54",
+    'x-rapidapi-host': "numbersapi.p.rapidapi.com"
+  }
+  response = requests.request("GET", url, headers=headers, params=querystring)
+  json_data = json.loads(response.text)
+  updateTrivia(json_data)
+  
+# adds trivia data to db
+def updateTrivia(trivia_data):
+  # if it already exists, updates it
+  if "trivia" in db.keys():
+    deleteTrivia()
+    db["trivia"] = trivia_data
 
+    # else, creates new entry in db
+  else:
+    db["trivia"] = trivia_data
+
+def deleteTrivia():
+  del db["trivia"]
 
 @client.event
 async def on_ready():
@@ -82,17 +91,36 @@ async def on_message(message):
     userNum = random.randrange(0, 100)
     botNum = random.randrange(0, 100)
     y = message.author.name
+
     await message.channel.send('\n' + y + ' rolled a ' + str(userNum) + '! ' + 'LaevusBot rolled a ' + str(botNum) + '!')
+
     if userNum > botNum:
       await message.add_reaction("LaevusPog:806694928092495913")
       await message.channel.send('Congratulations!')
     else:
       await message.channel.send('Sorry!')
-    
 
   if message.content.startswith('$stream'):
+    is_user_live()
     
-    await message.channel.send('cool')
+  if message.content.startswith('$trivia'):
+    getTrivia()
+    await message.channel.send(db["trivia"]['text'])
+
+  if message.content.startswith('$answer'):
+    msg = message.content
+    userAnswer = int(msg.split("$answer", 1)[1])
+    answer = db["trivia"]['number']
+
+    if userAnswer == answer:
+      await message.add_reaction("5Head:784646128209494016")
+
+    else:
+      await message.add_reaction("Pepega:784646222577139732")
+        
+    
+
+    
 
 keep_alive()
 client.run(my_secret)  
